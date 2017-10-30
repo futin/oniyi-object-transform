318b00a2a26c1ef4f17fd6630a74e799f45ab987
Benjamin Kröger <benjamin.kroeger@gis-ag.com>
2017-10-30 16:01:22 +0100

    updates dev dependencies

--- test/index.js
+++ test/index.js
@@ -1,72 +1,66 @@
-'use strict';
+// node core modules
 
-const assert = require('assert');
-const transform = require('../lib');
+// 3rd party modules
+import test from 'ava';
 
-describe('oniyi-object-transform', function () {
-  it('should throw when called without params argument', function () {
-    assert.throws(
-      transform,
-      /params argument is required/
-    );
-  });
+// internal modules
+import transform from '../lib';
 
-  it('should throw when called with no `source` or `src` param', function () {
-    assert.throws(
-      () => {
-        transform({});
-      },
-      /either\s`params.source`\sor\s`params.src`\smust\sbe\sprovided/
-    );
-  });
+test('should throw when called without params argument', (t) => {
+  t.throws(transform, /params argument is required/);
 });
 
-describe('oniyi-object-transform map', function () {
-  it('should rename property `foo` to `sports`', function () {
-    const source = {
-      foo: 'bar',
-      lorem: 'ipsum',
-    };
-
-    const transformed = transform({
-      source,
-      map: {
-        foo: 'sports',
-      },
-    });
-
-    assert(transformed.sports, source.foo, 'we expect `transformed.sports` to have the same value as `source.foo`');
-  });
+test('should throw when called with no `source` or `src` param', (t) => {
+  t.throws(() => {
+    transform({});
+  }, /either\s`params.source`\sor\s`params.src`\smust\sbe\sprovided/);
 });
 
-describe('oniyi-object-transform pick', function () {
-  it('should pick only property `foo` from source', function () {
-    const source = {
-      foo: 'bar',
-      lorem: 'ipsum',
-    };
+test('should rename property `foo` to `sports`', (t) => {
+  const source = {
+    foo: 'bar',
+    lorem: 'ipsum',
+  };
 
-    const transformed = transform({
-      source,
-      pick: ['foo'],
-    });
-
-    assert(Object.keys(transformed).length === 1, 'we expect `transformed` to only have one property');
-    assert(transformed.hasOwnProperty('foo'), 'we expect `transformed` to have own property `foo`');
-    assert.equal(transformed.foo, source.foo, 'we expect `transformed.foo` to equal `souroce.foo`');
+  const transformed = transform({
+    source,
+    map: {
+      foo: 'sports',
+    },
   });
 
-  it('should return empty object literal when `pick` param is empty array', function () {
-    const source = {
-      foo: 'bar',
-      lorem: 'ipsum',
-    };
+  t.is(transformed.sports, source.foo, 'we expect `transformed.sports` to have the same value as `source.foo`');
+});
 
-    const transformed = transform({
-      source,
-      pick: [],
-    });
+test('should pick only property `foo` from source', (t) => {
+  const source = {
+    foo: 'bar',
+    lorem: 'ipsum',
+  };
 
-    assert(Object.keys(transformed).length === 0, 'we expect `transformed` to have zero properties');
+  const transformed = transform({
+    source,
+    pick: ['foo'],
   });
+
+  t.is(Object.keys(transformed).length, 1, '`transformed` is expected to only have one property');
+  t.true(
+    Object.prototype.hasOwnProperty.call(transformed, 'foo'),
+    'we expect `transformed` to have own property `foo`'
+  );
+  t.is(transformed.foo, source.foo, 'we expect `transformed.foo` to equal `souroce.foo`');
+});
+
+test('should return empty object literal when `pick` param is empty array', (t) => {
+  const source = {
+    foo: 'bar',
+    lorem: 'ipsum',
+  };
+
+  const transformed = transform({
+    source,
+    pick: [],
+  });
+
+  t.is(Object.keys(transformed).length, 0, 'we expect `transformed` to have zero properties');
 });
